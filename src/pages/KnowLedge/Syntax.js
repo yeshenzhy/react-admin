f1a0370bb70b761713452f0a79ec83ce7c3ad4b9
张海印 <zhanghaiyin@vchangyi.com/>
2020-08-24 20:08:49 +0800

    测试jenkins

--- src/pages/KnowLedge/Syntax.js
+++ src/pages/KnowLedge/Syntax.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
+
 import Know from './know';
 
 class Syntax extends Component {
   constructor(props) {
     super(props);
+
     console.log(this.props);
   }
   render() {
-    const list = ['a', 'b', 'c'];
-    const dom = list.map((item, index) => (<li data={list} key={index}>{item}</li>)) || '';
+    const list = ['a', 'b', 'c', 'zhy'];
+
+    const dom = list.map((item, index) => { return (<li data={list} key={index}>{item}</li>); }) || '';
+
     console.log(this.props);
+
     return (
+
       <div>
+
         <Know>
+
           <div>
+
             我是button
+
             {' '}
+
           </div>
+
         </Know>
+
         <ul>{dom}</ul>
+
       </div>
 
     );
@@ -26,3 +40,4 @@ class Syntax extends Component {
 }
 
 export default Syntax;
+
