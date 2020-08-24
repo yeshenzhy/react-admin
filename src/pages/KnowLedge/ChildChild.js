cfb2313e349c41aaa7bf2fd6b01c5134f153142b
张海印 <zhanghaiyin@vchangyi.com/>
2020-08-24 19:56:23 +0800

    11

--- src/pages/KnowLedge/ChildChild.js
+++ src/pages/KnowLedge/ChildChild.js
@@ -5,7 +5,7 @@ export default class ChildChild extends React.Component {
   render() {
     return (
       <div className="box2">
-        <Button type="primary">去详情aaaaa</Button>
+        <Button type="primary">去详情aaaaaaaa</Button>
         <a href="#/knowLedge/syntax/detail">去详情</a>
         <p className="p">我是pzz标签</p>
       </div>
