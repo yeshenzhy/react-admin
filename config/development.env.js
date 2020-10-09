c5b237e5f7a8569ecad5b2a34a5a956da78a970f
张海印 <zhanghaiyin@vchangyi.com/>
2020-10-09 16:16:30 +0800

    修改接口地址

--- config/development.env.js
+++ config/development.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: 'http://localhost:8888/api/v1',
+  BASE_API: 'http://localhost:5678/api/v1',
 };
--- config/production.env.js
+++ config/production.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: 'http://106.12.173.13:8888/api/v1',
+  BASE_API: 'http://service.yeshen1.cn/api/v1',
 };
