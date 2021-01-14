3a1a05e306d330ffdfe3f7960c9cd1e7d4cf1fde
张海印 <zhanghaiyin@vchangyi.com/>
2021-01-14 18:39:12 +0800

    测试

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -40,7 +40,7 @@ class UploadImg extends React.Component {
         </p>
         <p className="ant-upload-text">Click or drag file to this area to upload</p>
         <p className="ant-upload-hint">
-      Support for a single or bulk upload.
+      Support for a single or bulk
         </p>
       </Dragger>
     );
