5da451071e2f84ca421203a4a3bd1a9220720f2d
张海印 <zhanghaiyin@vchangyi.com/>
2021-01-14 18:27:50 +0800

    1

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -40,8 +40,7 @@ class UploadImg extends React.Component {
         </p>
         <p className="ant-upload-text">Click or drag file to this area to upload</p>
         <p className="ant-upload-hint">
-      Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-      band files
+      Support for a single or bulk upload.
         </p>
       </Dragger>
     );
