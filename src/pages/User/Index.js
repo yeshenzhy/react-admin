69b2facaffcbcec8567ae5d2772fc5be5f09a9d4
张海印 <zhanghaiyin@vchangyi.com/>
2021-01-24 15:29:44 +0800

    用户中心数据

--- src/pages/User/Index.js
+++ src/pages/User/Index.js
@@ -5,8 +5,12 @@ import './index.scss';
 import emitter from '@src/utils/bus';
 
 const UploadAvatar = () => {
-  const { Dragger } = Upload;
-  const [loading, setLoading] = useState(false);
+  const [uploadResult, setUploadResult] = useState({ loading: false, imgUrl: '' });
+  const getBase64 = (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  };
   const props = {
     name: 'file',
     listType: 'picture-card',
@@ -21,33 +25,38 @@ const UploadAvatar = () => {
     onChange(info) {
       const { status, response } = info.file;
       if (status === 'uploading') {
-        setLoading(true);
+        setUploadResult((prev) => ({ ...prev, loading: true }));
       }
       if (status === 'done') {
         const userInfo = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : {};
         userInfo.avatar = response.data.url; 
         emitter.emit('updateUserInfo', userInfo);
         message.success('图片上传成功');
-        setLoading(false);
+        getBase64(info.file.originFileObj, imgUrl => setUploadResult({ imgUrl, loading: false }));
       } else if (status === 'error') {
         message.error('图片上传失败');
-        setLoading(false);
+        setUploadResult((prev) => ({ ...prev, loading: false }));
       }
     },
   };
+  const uploadButton = (
+    <div>
+      <Icon type={uploadResult.loading ? 'loading' : 'plus'} />
+      <div className="ant-upload-text">上传头像</div>
+    </div>
+  );
   return (
     <div className="avatar-upload">
-      <Dragger {...props}>
-        <div>
-          <Icon type={loading ? 'loading' : 'plus'} />
-          <div className="ant-upload-text">上传头像</div>
-        </div>
-      </Dragger>
+      <Upload {...props}>
+        { uploadResult.imgUrl ? <img alt="avatar" src={uploadResult.imgUrl} style={{ width: '100%', maxHeight: '86px' }} /> : uploadButton }
+      </Upload>
     </div>
   );
 };
+
 const UserCenter = (props) => {
   const { getFieldDecorator } = props.form;
+  const [confirmDirty, setConfirmDirty] = useState(false);
   const formItemLayout = {
     labelCol: {
       xs: { span: 24 },
@@ -70,61 +79,108 @@ const UserCenter = (props) => {
       },
     },
   };
+  // 自定义密码校验
   const validateToNextPassword = (rule, value, callback) => {
     const { form } = props;
-    if (value) {
+    if (value && confirmDirty) {
       form.validateFields(['confirm'], { force: true });
     }
     callback();
   };
+  // 自定义再次密码校验
   const compareToFirstPassword = (rule, value, callback) => {
     const { form } = props;
     if (value && value !== form.getFieldValue('password')) {
-      callback('Two passwords that you enter is inconsistent!');
+      callback('再次确认密码和输入密码不一样！');
     } else {
       callback();
     }
   };
-  const handleConfirmBlur = e => {
-    console.log(e);
-    // const { value } = e.target;
-    // this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  // 提交表单
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.form.validateFieldsAndScroll((err, values) => {
+      console.log(values, '1111');
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
   };
+  // 表单数去焦点
+  const handleConfirmBlur = e => {
+    const { value } = e.target;
+    setConfirmDirty((prev) => prev || !!value);
+  };
+  // 表单配置项
+  const formConfig = () => [
+    { 
+      itemOptions: { label: '用户头像', extra: '点击头像上传图片' },
+      initialValue: '', 
+      rules: [],
+      name: '',
+      children: <UploadAvatar></UploadAvatar>, 
+    },
+    { 
+      itemOptions: { label: '用户昵称', hasFeedback: true },
+      initialValue: '夜神', 
+      rules: [
+        {
+          required: true,
+          max: 8,
+          message: '请输入用户昵称',
+        },
+      ],
+      name: 'nick',
+      children: <Input placeholder="请输入用户昵称" />, 
+    },
+    { 
+      itemOptions: { label: '用户手机号', extra: '用户手机号不可修改' },
+      initialValue: '18345456789', 
+      rules: [],
+      name: 'mobile',
+      children: <Input disabled />, 
+    },
+    { 
+      itemOptions: { label: '用户登录密码', hasFeedback: true },
+      initialValue: '', 
+      rules: [
+        {
+          required: true,
+          message: '请输入密码',
+        },
+        {
+          required: true,
+          max: 15,
+          message: '密码长度为6~15位',
+        },
+        { validator: validateToNextPassword },
+      ],
+      name: 'password',
+      children: <Input.Password placeholder="请输入密码" />, 
+    },
+    { 
+      itemOptions: { label: '再次确认密码', hasFeedback: true },
+      initialValue: '', 
+      rules: [
+        {
+          required: true,
+          message: '请输入再次确认密码',
+        },
+        { validator: compareToFirstPassword },
+      ],
+      name: 'confirm',
+      children: <Input.Password placeholder="请输入再次确认密码" onBlur={handleConfirmBlur} />,
+    },
+  ];
   return (
     <Row>
       <Col span={12}>
-        <Form {...formItemLayout} className="user-center">
-          <Form.Item label="用户头像" extra="点击头像上传图片">
-            <UploadAvatar></UploadAvatar>
-          </Form.Item>
-          <Form.Item label="用户昵称">
-            <Input placeholder="请输入用户昵称" />
-          </Form.Item>
-          <Form.Item label="用户手机号" extra="用户手机号不可修改">
-            <Input value="13456545678" disabled />
-          </Form.Item>
-          <Form.Item label="用户登录密码" hasFeedback>
-            {getFieldDecorator('password', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please input your password!',
-                },
-                { validator: validateToNextPassword },
-              ],
-            })(<Input.Password placeholder="请输入密码" />)}
-          </Form.Item>
-          <Form.Item label="再次确认密码" hasFeedback>
-            {getFieldDecorator('confirm', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please confirm your password!',
-                },
-                { validator: compareToFirstPassword },
-              ],
-            })(<Input.Password placeholder="请输入再次确认密码" onBlur={handleConfirmBlur} />)}
-          </Form.Item>
+        <Form {...formItemLayout} onSubmit={handleSubmit} className="user-center">
+          {formConfig().map(({ itemOptions, children, name, rules, initialValue }, i) => (
+            <Form.Item {...itemOptions} key={i}>
+              { name ? getFieldDecorator(name, { rules, initialValue })(children) : children}
+            </Form.Item>
+          ))}
           <Form.Item {...tailFormItemLayout}>
             <Button className="mar-r-10">取消</Button>
             <Button type="primary" htmlType="submit">保存</Button>
